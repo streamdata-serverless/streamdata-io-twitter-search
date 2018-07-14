22484f6814c7add23e8a19739090551dae45b0ff
Kin Lane <kinlane@gmail.com>
2018-07-14 15:26:15 -0400

    add package and README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# streamdata-io-twitter-search
-Streamdata.io Twitter Search Streaming to Amazon S3 Data Lake Using Lambda
+# Streamdata.io Twitter Search Streaming to AWS S3 Data Lake (Serverless)
+<img src="https://s3.amazonaws.com/kinlane-productions/streamdata/streamdata-logo-vertical-2.png" align="right" width="250" />This is a demo of delivering a stream of a Twitter search using Streamdata.io running on AWS Lambda as a "serverless" function. It is designed to take this existing JSON API and turn it into a streaming API using Server-Sent Events (SSE), applying incremental updates using JSON Patch, and then stream the results into a data lake store on Amazon S3.
+
+You will need a Streamdata.io account and application key to run this function, Twitter account and application, as well as an AWS account to run the Lambda function in, and AWS S3 to store the data. You can run this as stream for up to five minutes (the maximum timeout of a Lambda script), then rerun every X minutes, or other schedule using AWS CloudWatch Events. Providing an existing API stream that can be turned on or off as needed, delivering real time updates to your Amazon S3 data lake.
+
+## Key Links:
+- [Streamdata.io Service](http://streamdata.io)
+- [Streamdata.io Account Signup](https://portal.streamdata.io/)
+- [Twitter API](https://developer.twitter.com/en.html)
+- [AWS Serverless Application Repository](https://serverlessrepo.aws.amazon.com/#/applications/arn:aws:serverlessrepo:us-east-1:879370021840:applications~StreamData-IO-Twitter-Search)
+- [License: MIT License](https://github.com/streamdata-serverless/streamdata-io-basic-demo-stockmarket-prices/blob/master/LICENSE)
+
+## Frequently Asked Questions:
+
+- Where do I put my Streamdata.io key? In the appToken environment variable available in the settings for your function after you have installed in.
+- Where does the data end up in Amazon S3? There are two environment variables for the function: 1) s3bucket, and 2) targetFolder, which will define your "data lake" destination on Amazon S3 -- you will need to create the bucket, but the folder will automatically be created.
+- Where do I get support for this function? Submit your issues via [the Github repository for this project](https://github.com/streamdata-serverless/streamdata-io-twitter-search/issues)
+- Is Twitter OAuth token required? Yes
+- How do I get a Twitter OAuth Token? You will need to setup an application, and perform the OAuth dance for your application. We could generate one for you, but then we'd have access to your token.
---
+++ package/index.js
@@ -0,0 +1,125 @@
+exports.handler = (event, context, callback) => {
+    
+    // Streamdata Dependencies
+    var streamdataio = require('streamdataio-js-sdk/dist/bundles/streamdataio-node');
+    var AuthStrategy = require('streamdataio-js-sdk-auth');
+    
+    // All The Other Dependencies
+    var jsonPatch = require('fast-json-patch');
+    var print = require('node-print');
+    var AWS = require('aws-sdk');
+    var dateTime = require('node-datetime');
+    
+    // Function for writing to S3
+    function putObjectToS3(s3bucket, s3key, data){
+    var s3 = new AWS.S3();
+        var params = {
+            Bucket : s3bucket,
+            Key : s3key,
+            Body : data
+        }
+        s3.putObject(params, function(err, data) {
+          if (err) console.log(err, err.stack); // an error occurred
+          else     console.log(data);           // successful response
+        });
+        }
+    
+    function server()
+    {
+      // targetUrl is the JSON API you wish to stream
+      var targetUrl = process.env.targetUrl;
+      
+      // q is the query parameter to search by
+      var query = process.env.query;
+      
+      targetUrl = targetUrl + '?q=' + query;
+      
+      // s3bucket i sthe bucket you wish write data into
+      var s3bucket = process.env.s3bucket;
+      
+      // targetFolder is the folder you wish to write data into
+      var targetFolder = process.env.targetFolder;
+      
+      // appToken is the Streamdata.io token
+      var appToken = process.env.appToken;
+      
+      // oauthToken is the Twitter Oauth Token
+      var oauthToken = process.env.oauthToken;
+      
+      // userAgent is used to identify your client
+      var userAgent = process.env.userAgent;      
+      
+      var privateKey = '';
+      
+      var headers = ['Authorization: Bearer ' + oauthToken,'User-Agent: ' + userAgent];
+    
+      var eventSource = streamdataio.createEventSource(targetUrl, appToken, headers, AuthStrategy.newSignatureStrategy(appToken, privateKey));
+      var result = [];
+    
+      eventSource
+      // the standard 'open' callback will be called when connection is established with the server
+        .onOpen(function ()
+        {
+          console.log("connected!");
+        })
+        // the streamdata.io specific 'data' event will be called when a fresh Json data set
+        // is pushed by Streamdata.io coming from the API
+        .onData(function (data)
+        {
+          console.log("data received");
+          // memorize the fresh data set
+          
+          result = data;
+          console.log(result);
+          
+          //putObjectToS3(s3bucket, s3key, result);
+          
+          // write file name with time stamp
+          var dt = dateTime.create();
+          var formatted = dt.format('Y-m-d-H-M-S');
+          var file = targetFolder + '/' + formatted + '.json';
+          
+          // put the entire result to S3
+          putObjectToS3(s3bucket, file, JSON.stringify(result));
+          
+        })
+        // the streamdata.io specific 'patch' event will be called when a fresh Json patch
+        // is pushed by streamdata.io from the API. This patch has to be applied to the
+        // latest data set provided.
+        .onPatch(function (patch)
+        {
+          // display the patch
+          console.log("patch: ", patch);
+          
+          // apply the patch to data using json patch API
+          jsonPatch.applyPatch(result, patch);
+          
+          // write file name with time stamp
+          var dt = dateTime.create();
+          var formatted = dt.format('Y-m-d-H-M-S');
+          var file = targetFolder + '/' + formatted + '.json';
+       
+          // put the entire result to S3
+          putObjectToS3(s3bucket, file, JSON.stringify(result));
+    
+        })
+    
+        // the standard 'error' callback will be called when an error occur with the evenSource
+        // for example with an invalid token provided
+        .onError(function (error)
+        {
+          console.log('ERROR!', error);
+          eventSource.close();
+    
+        });
+    
+      eventSource.open();
+    
+    }
+    
+    console.log('starting');
+    server();
+
+    // TODO implement
+    callback(null, 'Hello from Lambda');
+};
